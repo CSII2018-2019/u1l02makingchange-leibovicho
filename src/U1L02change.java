d5bf630da0818c8ed49e65f27cd6e97bbdc106b3
leibovicho <37046788+leibovicho@users.noreply.github.com>
2018-09-13 09:18:53 -0400

    Code for nickles and pennies (using while loop)

--- src/U1L02change.java
+++ src/U1L02change.java
@@ -1,3 +1,4 @@
+import javax.swing.JOptionPane;
 
 public class U1L02change {
 
@@ -5,22 +6,27 @@ public class U1L02change {
 	
 /*Take input and convert to int: “How much cash do you have?”
 Multiply by 100
-
 Set nickels = 0
 Set pennies = 0
-
 while cash > 5:
-
 cash = cash - 5
 nickels += nickels
-
 while cash > 0:
-
-cash = cash - 1
-pennies += pennies
-
+pennies = cash
 print(“You have “ + nickels + “nickels and “ + pennies + “ pennies.”)*/
-
+	int nickles = 0;
+	int pennies = 0;
+		
+	String answer = JOptionPane.showInputDialog("Enter a monetary value: ");
+	double cash = Double.parseDouble(answer);
+	pennies = (int)(cash * 100);
+	
+	while (pennies > 5) {
+		pennies -= 5;
+		nickles ++;
+	}
+	JOptionPane.showMessageDialog(null, cash + " is " + nickles +
+	" nickles and " + pennies + " pennies.");
 	}
 
 }
