a586f81852d5a67424ca04ac3f70f43d4b45925a
leibovicho <37046788+leibovicho@users.noreply.github.com>
2018-09-13 08:39:39 -0400

    pseudo

---
+++ src/U1L02change.java
@@ -0,0 +1,26 @@
+
+public class U1L02change {
+
+	public static void main(String[] args) {
+	
+/*Take input and convert to int: “How much cash do you have?”
+Multiply by 100
+
+Set nickels = 0
+Set pennies = 0
+
+while cash > 5:
+
+cash = cash - 5
+nickels += nickels
+
+while cash > 0:
+
+cash = cash - 1
+pennies += pennies
+
+print(“You have “ + nickels + “nickels and “ + pennies + “ pennies.”)*/
+
+	}
+
+}
